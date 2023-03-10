02c69a72d4e3c1bb11b4ead35b7a11fad843f38b
OneClock1 <toly1539@gmail.com>
2023-03-10 23:47:16 +0200

    Create Header.scss

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Header from './components/shared/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>Dawnstar</h1>
     </div>
   );
